dfb5590b1788e6a935204db8c5432ae99e6f2520
Manoel Vieira Lima Junior <manoel.lima03@gmail.com>
2022-09-08 18:58:00 -0300

    Resolvendo conflitos

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,90 +1,11 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import logo from '../trivia.png';
-import { addNameEmail } from '../redux/actions';
-
-class Login extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      name: '',
-      email: '',
-      disableButton: true,
-    };
-  }
-
-  handleChange = ({ target }) => {
-    const { id, value } = target;
-    this.setState({
-      [id]: value,
-    }, this.handleValidation);
-  };
-
-  handleValidation = () => {
-    const { name, email } = this.state;
-    if (name.length !== 0 && email.length !== 0) {
-      this.setState({ disableButton: false });
-    } else {
-      this.setState({ disableButton: true });
-    }
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const { dispatch, history } = this.props;
-    const { name, email } = this.state;
-    dispatch(addNameEmail({
-      name,
-      email,
-    }));
-    history.push('/games');
-  };
+import LoginForm from '../components/LoginForm';
 
 export default class Login extends Component {
   render() {
     return (
-
-      <div>
-        <img src={ logo } className="App-logo" alt="logo" />
-        <form>
-          <input
-            id="name"
-            value={ name }
-            type="text"
-            data-testid="input-player-name"
-            onChange={ this.handleChange }
-            autoComplete="off"
-          />
-          <input
-            id="email"
-            value={ email }
-            type="email"
-            data-testid="input-gravatar-email"
-            onChange={ this.handleChange }
-            autoComplete="off"
-          />
-          <button
-            disabled={ disableButton }
-            type="submit"
-            data-testid="btn-play"
-            onClick={ this.handleSubmit }
-          >
-            Play
-
-          </button>
-        </form>
-      </div>
       <LoginForm { ...this.props } />
 
     );
   }
 }
-
-Login.propTypes = {
-  dispatch: PropTypes.func,
-}.isRequired;
-
-export default connect()(Login);
-
